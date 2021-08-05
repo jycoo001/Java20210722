ade7cd097e7843dd67bbb30736870000d594876d
jycoo <9479968+jycoo@user.noreply.gitee.com>
2021-08-05 10:42:32 +0800

    消除--

--- Java20210722/One/src/com/situ/three/TestThree.java
+++ Java20210722/One/src/com/situ/three/TestThree.java
@@ -8,7 +8,24 @@ public class TestThree {
     @Test
     public void test1 () {
         Scanner st = new Scanner(System.in);
-        String str = st.next();
-        System.out.println(str);
+        String str = st.nextLine();
+        String str1 = trim(str);
+        System.out.println(str1);
+    }
+    public String trim (String str) {
+        try {
+            int start = 0;
+            int end = str.length() - 1;
+            while ((str.charAt(start) == '-') && (start <= end)) {
+                start++;
+            }
+            while ((start <= end) && (str.charAt(end) == '-')) {
+                end--;
+            }
+            return str.substring(start, end+1);
+        }catch (Exception e) {
+            return e.getMessage();
+        }
+
     }
 }
