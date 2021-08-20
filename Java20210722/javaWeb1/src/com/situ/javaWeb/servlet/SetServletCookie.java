96790717d2e045612d54ec116c563229df8b807b
jycoo <9479968+jycoo@user.noreply.gitee.com>
2021-08-20 12:37:17 +0800

    cookie-lasttime

---
+++ Java20210722/javaWeb1/src/com/situ/javaWeb/servlet/SetServletCookie.java
@@ -0,0 +1,20 @@
+package com.situ.javaWeb.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/SetCookie")
+public class SetServletCookie extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("UTF-8");
+        Cookie cookie = new Cookie("abc","123456");
+        resp.addCookie(cookie);
+
+    }
+}
