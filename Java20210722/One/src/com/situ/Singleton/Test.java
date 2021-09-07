59299cc14e5c0d94843b9321fd2601fe238df1ce
jycoo <1243094603@qq.com>
2021-09-07 02:11:35 +0000

    删除文件 Java20210722/One

--- Java20210722/One/src/com/situ/Singleton/Test.java
+++
@@ -1,8 +0,0 @@
-package com.situ.Singleton;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(Singleton.getInstance());
-        System.out.println(Singleton1.getInstance());
-    }
-}
--- Java20210722/One/src/com/situ/fff/Sql/IDB.java
+++
@@ -1,5 +0,0 @@
-package com.situ.fff.Sql;
-
-public interface IDB {
-    public abstract void getConnection();
-}
